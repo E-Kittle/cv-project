369203b97fc33fbab515bd3acdadb6939ac111b7
Elisabeth Kittle <elisabethkittle@gmail.com>
2021-05-26 16:03:39 -0400

    Added basic information for skills section

--- src/components/FormManager.js
+++ src/components/FormManager.js
@@ -12,23 +12,23 @@ class FormManager extends React.Component {
     render() {
         return (
             <div className="container">
-                <div>
+                <div className="containerSection">
                     <h2 className="formSection">Personal Information</h2>
                     <PersonalForm handleNameChange={this.props.handleNameChange} />
                     {/* Personal Info Component - Always loads */}
                 </div>
 
-                <div>
+                <div className="containerSection">
                     <h2>Employment Experience</h2>
                     {/* Dynamically load employment information - Use {employment} to create an array in the render section. This allows me to add additional employment as needed */}
                 </div>
 
-                <div>
+                <div className="containerSection">
                     <h2>Education</h2>
                     {/* Dynamically add education */}
                 </div>
 
-                <div>
+                <div className="containerSection">
                     <h2>Skills</h2>
                     {/* Dynamically add skills */}
                 </div>
